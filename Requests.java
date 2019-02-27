bdd14f45d3942a323c87f0ec9f379063df1e6a5f
Jason Madden <jason.madden8@mail.dcu.ie>
2019-02-27 01:09:13 +0000

    adding the requests class

---
+++ Requests.java
@@ -0,0 +1,10 @@
+// structure to model requests for the hashmap
+// since now there is no real "Person" object
+import java.util.*;
+
+public class Requests {
+    public int dest;
+    public Requests() {
+                
+    }
+}
